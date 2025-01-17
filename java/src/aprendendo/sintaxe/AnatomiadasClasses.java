c9939381074152aab27cf2b4513dca819a9e964d
Vagner-Alves <vagneralves997@gmail.com>
2025-01-17 16:58:26 -0300

    sintaxe java

--- java/src/aprendendo/sintaxe/AnatomiadasClasses.java
+++ java/src/aprendendo/sintaxe/AnatomiadasClasses.java
@@ -4,6 +4,11 @@ public class AnatomiadasClasses {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello world!");
+		
+		String BR = "Brasil";
+		double PI = 3.14;
+		int NUMERO_DE_ESTADOS_DO_BRASIL = 27;
+		
 
 	}
 

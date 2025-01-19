e82ad67039dd5da113105c63b2f4ec35aea4dac5
Vagner-Alves <vagneralves997@gmail.com>
2025-01-18 23:26:05 -0300

    tipos e variaveis

--- java/src/aprendendo/sintaxe/TipoDados.java
+++ java/src/aprendendo/sintaxe/TipoDados.java
@@ -2,6 +2,12 @@ package aprendendo.sintaxe;
 
 public class TipoDados {
 	public static void main(String [] args) {
+		byte idade      = 123;
+		short ano       = 2025;
+		int cep         = 21070333;
+		long cpf        = 98685039201L;
+		float pi        = 3.14F;
+		double salario  = 1518.44;
 		
 		
 	}

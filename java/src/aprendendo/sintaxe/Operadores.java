d5f68dd4c07307414314a4a669d3d18ba5d3858b
Vagner-Alves <vagneralves997@gmail.com>
2025-01-20 22:06:23 -0300

    operadores em java

--- java/src/aprendendo/sintaxe/Operadores.java
+++ java/src/aprendendo/sintaxe/Operadores.java
@@ -42,8 +42,47 @@ public class Operadores {
 		System.out.println( ++ numero);
 		
 		boolean verdadeiro = true;
-		
 		System.out.println("inverter" + !verdadeiro);
+		
+		// ou
+		boolean falso = !verdadeiro;
+		System.out.println(falso);
+		
+		// Operadores Ternários
+		
+		int a, b;
+		String resultadoCondicional, resultadoTernario;
+		a = 5;
+		b = 6;
+		
+		// Exemplo condicional utilizando uma estrutura if / else
+		 if ( a == b)
+			 resultadoCondicional = "verdadeiro";
+		 else
+			 resultadoCondicional = "falso";
+		
+		 // exemplo usando ternário
+		 resultadoTernario = (a==b) ? "verdadeiro" : "false" ;
+		 
+		 // Operadores Relacionais
+		 
+		 int numero1 = 1;
+		 int numero2 = 2;
+		 
+		 if (numero1 > numero2)
+			 System.out.println(numero1 + " é maior que " + numero2);
+		
+		 if (numero1 < numero2)
+			 System.out.println(numero1 + " é menor que " + numero2);
+		 
+		 if (numero1 >= numero2)
+			 System.out.println(numero1 + " é maior ou igual a " + numero2);
+		 
+		 if(numero1 <= numero2)
+			 System.out.println(numero1 + " é menor ou igual a " + numero2);
+		 
+		 if (numero1 != numero2)
+			 System.out.println(numero1 + " é diferente de " + numero2);
 	}
 
 }

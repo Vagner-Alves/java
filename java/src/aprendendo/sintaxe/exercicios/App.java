f7f8c50a75e5f8fbe50ff837c161f68ac51b1549
Vagner-Alves <vagneralves997@gmail.com>
2025-01-25 14:01:05 -0300

    exercicio smart tv

--- java/src/aprendendo/sintaxe/exercicios/App.java
+++ java/src/aprendendo/sintaxe/exercicios/App.java
@@ -3,6 +3,10 @@ package aprendendo.sintaxe.exercicios;
 public class App {
 	public static void main(String [] args) {
 		
+		SmartTv tv = new SmartTv();
+		
+		tv.ligar();
+		tv.ligar();
 	}
 
 }
--- java/src/aprendendo/sintaxe/exercicios/SmartTv.java
+++ java/src/aprendendo/sintaxe/exercicios/SmartTv.java
@@ -4,18 +4,20 @@ public class SmartTv {
 	boolean ligada = true;
 	int canal = 1;
 	int volume = 0;
-	
+
 	public void ligar() {
-		
+		ligada = (ligada == true) ? false: true;
+		System.out.println(ligada);
+
 	}
-	
+
 	public void aumentarVolume() {}
-	
+
 	public void diminuirVolume() {}
-	
+
 	public void mudarCanal (int numeroCanal) {
 		canal = numeroCanal;
 	}
-	
+
 
 }

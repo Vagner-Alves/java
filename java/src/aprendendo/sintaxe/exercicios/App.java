df212eb4da7b36e053c1586a44fe3f837f026d74
Vagner-Alves <vagneralves997@gmail.com>
2025-01-25 12:56:11 -0300

    exercicio smart tv

---
+++ java/src/aprendendo/sintaxe/exercicios/App.java
@@ -0,0 +1,8 @@
+package aprendendo.sintaxe.exercicios;
+
+public class App {
+	public static void main(String [] args) {
+		
+	}
+
+}
---
+++ java/src/aprendendo/sintaxe/exercicios/SmartTv.java
@@ -0,0 +1,8 @@
+package aprendendo.sintaxe.exercicios;
+
+public class SmartTv {
+	
+	
+	
+
+}

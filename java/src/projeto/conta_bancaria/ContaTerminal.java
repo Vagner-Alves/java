388506ebdaf75d79ca243a0df8b62d378ba4b055
Vagner-Alves <vagneralves997@gmail.com>
2025-02-03 16:56:19 -0300

    projeto conta bancaria

--- java/src/projeto/conta_bancaria/ContaTerminal.java
+++ java/src/projeto/conta_bancaria/ContaTerminal.java
@@ -1,9 +1,31 @@
 package projeto.conta_bancaria;
 
+import java.util.Scanner;
+
 public class ContaTerminal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int numero;
+		String agencia, nomeCliente;
+		double saldo;
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		// entrada de dados
+		System.out.println("por favor, informe o número de sua conta: ");
+		numero = teclado.nextInt();
+		
+		System.out.println("informe o número de sua agencia: ");
+		agencia = teclado.nextLine();
+		
+		System.out.println("informe seu Nome: ");
+		nomeCliente = teclado.nextLine();
+		
+		System.out.println("informe seu saldo: ");
+		saldo = teclado.nextDouble();
+		
+		
 
 	}
 

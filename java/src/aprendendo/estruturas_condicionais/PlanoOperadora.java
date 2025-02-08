b7e9df9e6571fcf7f9d5a4817237ecc7818722ea
Vagner-Alves <vagneralves997@gmail.com>
2025-02-08 10:36:50 -0300

    switch case

---
+++ java/src/aprendendo/estruturas_condicionais/PlanoOperadora.java
@@ -0,0 +1,26 @@
+package aprendendo.estruturas_condicionais;
+
+public class PlanoOperadora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String plano = "M";
+		
+		switch(plano) {
+		case "T": {
+			System.out.println("5gb Youtube");
+		}
+		
+		case "M": {
+			System.out.println("Whatsapp e instagram grátis");
+		}
+		
+		case "B": {
+			System.out.println("100 Minuto de ligação gratuitas");
+		}
+		
+		}
+
+	}
+
+}
--- java/src/aprendendo/estruturas_condicionais/SistemaMedida.java
+++ java/src/aprendendo/estruturas_condicionais/SistemaMedida.java
@@ -4,7 +4,7 @@ public class SistemaMedida {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String sigla = "M";
+		String sigla = "G";
 		
 		switch(sigla) {
 		

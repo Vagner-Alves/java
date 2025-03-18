2b8b9671d8bedd0fac79165743e7c7faf4a64c21
Vagner-Alves <vagneralves997@gmail.com>
2025-03-18 09:52:02 -0300

    desafio candidatura

---
+++ java/src/projeto/candidatura/ProcessoSeletivo.java
@@ -0,0 +1,19 @@
+package projeto.candidatura;
+
+public class ProcessoSeletivo {
+
+	public static void main(String[] args) {
+		System.out.println("Processo Seletivo: ");
+		
+
+	}
+	
+	static void analisarCandidato(double salarioPretendido) {
+		double salarioBase = 2000.0;
+		
+		if( salarioBase > salarioPretendido) {
+			System.out.println("Ligar para o Candidato");
+		}
+	}
+
+}

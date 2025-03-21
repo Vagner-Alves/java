31193e2dc0b4041c1a79a3c75697c4f5bef508eb
Vagner-Alves <vagneralves997@gmail.com>
2025-03-21 20:52:40 -0300

    orientação a objetos

---
+++ java/src/aprendendo/orientacao_objetos/Autodromo.java
@@ -0,0 +1,10 @@
+package aprendendo.orientacao_objetos;
+
+public class Autodromo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ java/src/aprendendo/orientacao_objetos/Carro.java
@@ -0,0 +1,17 @@
+package aprendendo.orientacao_objetos;
+
+public class Carro {
+	
+	public void ligar() {
+		System.out.println("Carro ligado.");
+	}
+	
+	public void conferirCombustivel() {
+		System.out.println("conferindo combustivel do veiculo");
+	}
+	
+	public void conferirCambio() {
+		System.out.println("Conferindo o cambio");
+	}
+
+}

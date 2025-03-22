74961ae7d476653e0d3b787b583d4acb50e70dc4
Vagner-Alves <vagneralves997@gmail.com>
2025-03-21 21:00:09 -0300

    orientação a objetos

--- java/src/aprendendo/orientacao_objetos/Autodromo.java
+++ java/src/aprendendo/orientacao_objetos/Autodromo.java
@@ -4,6 +4,9 @@ public class Autodromo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Carro jeep = new Carro();
+		
+		jeep.ligar();
 
 	}
 
--- java/src/aprendendo/orientacao_objetos/Carro.java
+++ java/src/aprendendo/orientacao_objetos/Carro.java
@@ -3,14 +3,16 @@ package aprendendo.orientacao_objetos;
 public class Carro {
 	
 	public void ligar() {
+		conferirCambio();
+		conferirCombustivel();
 		System.out.println("Carro ligado.");
 	}
 	
-	public void conferirCombustivel() {
+	private void conferirCombustivel() {
 		System.out.println("conferindo combustivel do veiculo");
 	}
 	
-	public void conferirCambio() {
+	private void conferirCambio() {
 		System.out.println("Conferindo o cambio");
 	}
 

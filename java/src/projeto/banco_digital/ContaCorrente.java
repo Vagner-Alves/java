a755a3c4e9740d60be7639b7107a25af085a960e
Vagner-Alves <vagneralves997@gmail.com>
2025-03-22 21:37:13 -0300

    projeto banco

--- java/src/projeto/banco_digital/ContaCorrente.java
+++ java/src/projeto/banco_digital/ContaCorrente.java
@@ -1,5 +1,11 @@
 package projeto.banco_digital;
 
 public class ContaCorrente {
-
+	private int agencia;
+	private int numero;
+	private double saldo;
+	
+	public void sacar() {}
+	public void depositar() {}
+	
 }

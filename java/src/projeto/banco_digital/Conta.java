34f7f2af4080c9d85e565b6b0b5ac5c2ff5430df
Vagner-Alves <vagneralves997@gmail.com>
2025-03-22 21:48:39 -0300

    projeto conta bancaria

--- java/src/projeto/banco_digital/Conta.java
+++ java/src/projeto/banco_digital/Conta.java
@@ -1,8 +1,26 @@
 package projeto.banco_digital;
 
-public class Conta {
+public abstract class Conta implements IConta {
 	private int agencia;
 	private int numero;
 	private double saldo;
+	
+	@Override
+	public void sacar(double valor) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void depositar(double valor) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void transferir(double valor, Conta contaDestino) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 
 }
